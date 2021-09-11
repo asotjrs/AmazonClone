85d3e5b0ca93d4e93d4b74b9eba890e28e862e13
Djafer Abdelhadi <41378212+asotjrs@users.noreply.github.com>
2021-09-11 03:00:08 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={{fontSize:70}}>{"Helooooooooooo World"}</Text>
+
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
